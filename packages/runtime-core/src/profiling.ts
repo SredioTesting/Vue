dd6cbe1303e211d65c28b24115915f148a78c565
zhangenming <282126346@qq.com>
2022-01-09 23:22:08 +0800

    refactor(runtime-core): profiling.
    When called, __DEV__ is guaranteed to be true
    so can delete line`if (__DEV__ || __FEATURE_PROD_DEVTOOLS__) `

--- packages/runtime-core/src/profiling.ts
+++ packages/runtime-core/src/profiling.ts
@@ -1,24 +1,22 @@
 import { ComponentInternalInstance, formatComponentName } from './component'
-import { devtoolsPerfEnd, devtoolsPerfStart } from './devtools'
+import { devtoolsPerfStart, devtoolsPerfEnd } from './devtools'
 
-let supported: boolean
-let perf: any
+const perf = typeof window !== 'undefined' && window.performance
+const timer = perf ? () => perf.now() : Date.now
 
 export function startMeasure(
   instance: ComponentInternalInstance,
   type: string
 ) {
-  if (instance.appContext.config.performance && isSupported()) {
-    perf.mark(`vue-${type}-${instance.uid}`)
-  }
+  devtoolsPerfStart(instance, type, timer())
 
-  if (__DEV__ || __FEATURE_PROD_DEVTOOLS__) {
-    devtoolsPerfStart(instance, type, supported ? perf.now() : Date.now())
+  if (perf && instance.appContext.config.performance) {
+    perf.mark(`vue-${type}-${instance.uid}`)
   }
 }
 
 export function endMeasure(instance: ComponentInternalInstance, type: string) {
-  if (instance.appContext.config.performance && isSupported()) {
+  if (perf && instance.appContext.config.performance) {
     const startTag = `vue-${type}-${instance.uid}`
     const endTag = startTag + `:end`
     perf.mark(endTag)
@@ -31,22 +29,5 @@ export function endMeasure(instance: ComponentInternalInstance, type: string) {
     perf.clearMarks(endTag)
   }
 
-  if (__DEV__ || __FEATURE_PROD_DEVTOOLS__) {
-    devtoolsPerfEnd(instance, type, supported ? perf.now() : Date.now())
-  }
-}
-
-function isSupported() {
-  if (supported !== undefined) {
-    return supported
-  }
-  /* eslint-disable no-restricted-globals */
-  if (typeof window !== 'undefined' && window.performance) {
-    supported = true
-    perf = window.performance
-  } else {
-    supported = false
-  }
-  /* eslint-enable no-restricted-globals */
-  return supported
+  devtoolsPerfEnd(instance, type, timer())
 }
